fb69e834572e069323bc8ff4dee2769c5c15be81
SimplyNate <horiuchi.nathan@gmail.com>
2021-08-26 19:48:32 -1000

    Created responsive board

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,49 @@
 <template>
-    <div></div>
+    <div id="#app">
+        <board :container-height="containerHeight" />
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Board from '@/board/Board.vue';
 
 export default defineComponent({
     name: 'App',
     components: {
+        Board,
+    },
+    data() {
+        return {
+            containerHeight: 0,
+        };
+    },
+    mounted() {
+        this.containerHeight = window.innerHeight;
+        window.addEventListener('resize', this.onResize);
+    },
+    methods: {
+        onResize() {
+            this.containerHeight = window.innerHeight;
+        },
     },
 });
 </script>
 
 <style>
+body {
+    margin: 0;
+}
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    background-color: #222222;
+    color: ghostwhite;
+    border: 0;
+    padding: 0;
+    margin: 0;
+    height: 100vh;
 }
 </style>
---
+++ src/board/Board.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container-fluid board">
+        <div class="row m-0 p-0" v-for="(h, i) of height" v-bind:key="i">
+            <div class="col m-0 p-0 cell" :style="`height: ${cellHeight}px`" v-for="(w, j) of width" v-bind:key="`${i}-${j}`">
+                {{ w }}-{{ h }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Board',
+    props: {
+        currentBoardRepresentation: String,
+        containerHeight: {
+            type: Number,
+            default: 100,
+        },
+    },
+    computed: {
+        cellHeight() {
+            return this.containerHeight / this.height.length;
+        },
+    },
+    data() {
+        return {
+            width: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+            height: [8, 7, 6, 5, 4, 3, 2, 1],
+        };
+    },
+});
+</script>
+
+<style scoped>
+.board {
+    margin: 0;
+    padding: 0;
+}
+.cell {
+    border: 1px solid white;
+}
+</style>
