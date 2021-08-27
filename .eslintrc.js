891844ed4205b986bebcd159fcb76136f5698578
SimplyNate <horiuchi.nathan@gmail.com>
2021-08-26 16:50:11 -1000

    Fixed styling, updates packages, switched to ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,33 @@
 module.exports = {
     root: true,
+
     env: {
-        node: true
+        node: true,
     },
-    'extends': [
-        'plugin:vue/vue3-essential',
-        'eslint:recommended'
-    ],
+
     parserOptions: {
-        parser: 'babel-eslint'
+        ecmaVersion: 2020,
+        parser: '@typescript-eslint/parser',
     },
+
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-    }
-}
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: ['error', 4],
+        'comma-dangle': ['error', 'always-multiline'],
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        'space-before-function-paren': ['error', {
+            anonymous: 'always',
+            named: 'never',
+            asyncArrow: 'always',
+        }],
+        'brace-style': ['error', 'stroustrup'],
+    },
+
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/standard',
+        '@vue/typescript'
+    ],
+};
