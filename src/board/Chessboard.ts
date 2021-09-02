81486fa35fadc230bcf241a03946f2b7dc28cc88
SimplyNate <horiuchi.nathan@gmail.com>
2021-09-01 19:48:42 -1000

    Expanding on the pixi rendering

--- src/board/Chessboard.ts
+++ src/board/Chessboard.ts
@@ -3,12 +3,29 @@ import * as PIXI from 'pixi.js';
 export class Chessboard {
     private width: number;
     private height: number;
-    private app: PIXI;
+    private squareLength: number;
+    private app: PIXI.Application;
 
-    constructor(width, height): void {
+    constructor(target: Element, width: number, height: number) {
         this.width = width;
         this.height = height;
-        this.app = new PIXI.Application({ width: this.width, height: this.height });
+        this.app = new PIXI.Application({ width: this.width, height: this.height, antialias: true });
+        target.appendChild(this.app.view);
+        this.drawBoard();
+    }
+
+    drawBoard() {
+    }
+
+    drawSquare(x: number, y: number, color: number) {
+        const square = new PIXI.Graphics();
+        square.beginFill(color);
+        square.drawRect(x, y, this.squareLength, this.squareLength);
+        square.endFill();
+    }
+
+    drawPiece() {
+
     }
 
     setWidth(newWidth: number): void {
