aac1259610d068ad10d2ae1eead262320e317fb1
SimplyNate <horiuchi.nathan@gmail.com>
2021-09-02 19:43:15 -1000

    Added logic for loading the pieces

--- src/board/Chessboard.ts
+++ src/board/Chessboard.ts
@@ -1,25 +1,69 @@
 import * as PIXI from 'pixi.js';
+import { BoardMap } from '@/board/utils';
+
+interface Pieces {
+    [index: string]: PIXI.Sprite,
+}
 
 export class Chessboard {
-    private containerWidth: number;
-    private containerHeight: number;
+    private parentWidth: number;
+    private parentHeight: number;
+    private containerLength: number;
     private squareLength: number;
+    private boardContainer: PIXI.Container;
     private app: PIXI.Application;
+    private pieces: Pieces = {};
+    private readonly row = [8, 7, 6, 5, 4, 3, 2, 1];
+    private readonly rank = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    private readonly pieceMap = {
+        66: 'B',
+        75: 'K',
+        78: 'N',
+        80: 'P',
+        81: 'Q',
+        82: 'R',
+        98: 'b',
+        107: 'k',
+        110: 'n',
+        112: 'p',
+        113: 'q',
+        114: 'r',
+    };
 
     constructor(target: Element, width: number, height: number, pixelRatio: number = 1) {
-        this.containerWidth = width;
-        this.containerHeight = height;
+        this.parentWidth = width;
+        this.parentHeight = height;
         this.app = new PIXI.Application({
-            width: this.containerWidth,
-            height: this.containerHeight,
+            width: this.parentWidth,
+            height: this.parentHeight,
             antialias: true,
             resolution: pixelRatio,
         });
+        this.loadSprites();
+        const container = new PIXI.Container();
+        this.containerLength = width > height ? height : width;
+        this.squareLength = this.containerLength / this.row.length;
+        container.x = this.app.screen.width / 2;
+        container.y = this.app.screen.height / 2;
+        this.app.stage.addChild(container);
+        this.boardContainer = container;
         target.appendChild(this.app.view);
         this.drawBoard();
     }
 
     drawBoard() {
+
+    }
+
+    loadSprites() {
+        for (const piece of Object.keys(this.pieceMap)) {
+            const texture = PIXI.Texture.from('../assets/pieces/' + piece + '.svg');
+            this.pieces[piece] = new PIXI.Sprite(texture);
+        }
+    }
+
+    placePieces(boardMap: BoardMap) {
+
     }
 
     drawSquare(x: number, y: number, color: number) {
