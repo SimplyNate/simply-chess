301d64e5e6ea3b44bfea595af8bb29e9a04232fa
Simply Nate <horiuchi.nathan@gmail.com>
2021-10-20 20:44:07 -1000

    Implemented additional properties, started process for move generation for Pawn

--- src/engine/Pieces.ts
+++ src/engine/Pieces.ts
@@ -1,10 +1,13 @@
+import { FEN, BoardMap, separateFEN } from '@/board/utils';
+
 type Color = 'light' | 'dark';
 
 interface Piece {
+    encoding: number;
     name: string;
     color: Color;
     position: string | null;
-    getLegalMoves(): string[];
+    getLegalMoves(currentBoard: BoardMap): string[];
     get code(): string;
     move(position: string): void;
 }
@@ -13,22 +16,34 @@ export class Pawn implements Piece {
     name = 'pawn';
     color: Color;
     position: string | null;
+    rank: number;
+    file: string;
+    inStartingPosition: boolean;
 
-    constructor(color: Color, position = null) {
+    constructor(color: Color, position: string) {
         this.color = color;
         this.position = position;
+        const [file, rank] = position.split('-');
+        this.rank = Number(rank);
+        this.file = file;
+        this.inStartingPosition = (this.rank === 7 && this.color === 'dark') || (this.rank === 2 && this.color === 'light');
     }
 
     move(position: string): void {
         this.position = position;
     }
 
-    getLegalMoves(): string[] {
+    getLegalMoves(currentBoard: BoardMap): string[] {
+        const moves = [];
+        const direction = this.color === 'dark' ? -1 : 1;
+        if (this.inStartingPosition) {
+            moves.push();
+        }
         return [];
     }
 
     get code(): string {
-        return this.color === 'light' ? 'p' : 'P';
+        return this.color === 'dark' ? 'p' : 'P';
     }
 }
 export class Bishop implements Piece {}
