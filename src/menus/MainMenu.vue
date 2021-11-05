008c89e4e4edafc4a54dc7498e6d558106122c63
Simply Nate <horiuchi.nathan@gmail.com>
2021-11-04 18:21:05 -1000

    Added menu component

---
+++ src/menus/MainMenu.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button class="btn btn-success" @click="startGame">New Game</button>
+        <button class="btn btn-success" @click="startGame">Load Game</button>
+        <input class="form-control" type="text" v-model="load" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainMenu',
+    data() {
+        return {
+            load: null,
+        };
+    },
+    methods: {
+        startGame() {
+            const game = this.load ? this.load : 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1';
+            this.$emit('start', game);
+        },
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
