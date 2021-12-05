20158ed510ec7bbfb78e674e8595b9c428dcd2a8
Simply Nate <horiuchi.nathan@gmail.com>
2021-12-04 17:11:49 -1000

    Completed Pawn legal move calculator

--- src/engine/pieces/Pawn.ts
+++ src/engine/pieces/Pawn.ts
@@ -15,9 +15,27 @@ export default class Pawn extends Piece {
             const moves = [];
             const direction = this.color === 'dark' ? -1 : 1;
             if (this.inStartingPosition) {
-                moves.push(`${this.file}-${this.rank += (2 * direction)}`);
+                const move2 = `${this.file}-${this.rank + (2 * direction)}`;
+                if (currentBoard[move2] === 'x') {
+                    moves.push(move2);
+                }
             }
-            moves.push(`${this.file}-${this.rank += (1 * direction)}`);
+            const move1 = `${this.file}-${this.rank + (1 * direction)}`;
+            if (currentBoard[move1] === 'x') {
+                moves.push(move1);
+            }
+            if (fen.enPassantTargetSquare !== '-') {
+                const leftFile = String.fromCharCode(this.file.charCodeAt(0) - 1);
+                const rightFile = String.fromCharCode(this.file.charCodeAt(0) + 1);
+                const rank = this.rank + (1 * direction);
+                const possibleEPSquares = [`${leftFile}-${rank}`, `${rightFile}-${rank}`];
+                for (const possibleSquare of possibleEPSquares) {
+                    if (possibleSquare === fen.enPassantTargetSquare) {
+                        moves.push(possibleSquare);
+                        break; // There can only ever be one match
+                    }
+                }
+            }
             this.legalMoves = moves;
         }
         return this.legalMoves;
