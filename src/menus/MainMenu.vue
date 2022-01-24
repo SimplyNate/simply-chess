c2d616197d848084ef8bafd98ae14307eb0d94ce
SimplyNate <horiuchi.nathan@gmail.com>
2022-01-23 16:42:21 -1000

    Added UI to support additinal options for play

--- src/menus/MainMenu.vue
+++ src/menus/MainMenu.vue
@@ -5,18 +5,31 @@
             <div class="row">
                 <div class="col border border-light rounded-1 ms-3 me-3">
                     <h3 class="pt-3">Start a New Game</h3>
-                    <p class="pt-3">Start a new game as white.</p>
-                    <div style="margin-bottom: 1em;"><input type="checkbox" v-model="useAi" /> Use AI?</div>
+                    <p class="pt-3">Start a new game in default starting position.</p>
                     <button class="btn btn-success mb-3" @click="startGame">New Game</button>
                 </div>
                 <div class="col border border-light rounded-1 ms-3 me-3">
                     <h3 class="pt-3">Load a Game</h3>
                     <p class="pt-3">Load an existing game using a FEN string.</p>
-                    <div style="margin-bottom: 1em;"><input type="checkbox" v-model="useAi" /> Use AI?</div>
                     <div class="input-group mb-3">
                         <input class="form-control" type="text" v-model="load" placeholder="Input FEN string here." />
                         <button class="btn btn-success" @click="startGame">Load Game</button>
                     </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col border border-light rounded-1 ms-3 me-3 mt-3">
+                    <h3>Additional Settings</h3>
+                    <div>Play as:
+                        <select v-model="playAs">
+                            <option>light</option>
+                            <option>dark</option>
+                        </select>
+                    </div>
+                    <div style="margin-bottom: .1em;"><input type="checkbox" v-model="useAi" /> Use AI?</div>
+                    <select v-if="useAi" style="margin-bottom: .2em;" v-model="selectedAi">
+                        <option v-for="ai of aiList" :key="ai">{{ ai }}</option>
+                    </select>
                 </div>
             </div>
         </div>
@@ -32,6 +45,11 @@ export default defineComponent({
         return {
             load: null,
             useAi: false,
+            aiList: [
+                'RandomMover',
+            ],
+            selectedAi: '',
+            playAs: 'light',
         };
     },
     methods: {
@@ -44,7 +62,7 @@ export default defineComponent({
         startGame() {
             if (this.validateFen()) {
                 const game = this.load ? this.load : 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0';
-                this.$router.push(`/play?fen=${game}&ai=${this.useAi}`);
+                this.$router.push(`/play?fen=${game}&playAs=${this.playAs}&ai=${this.useAi}&aiType=${this.selectedAi}`);
             }
         },
     },
