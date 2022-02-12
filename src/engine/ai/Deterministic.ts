f36277f140a9898b262096c0d6f600a7ef06c467
SimplyNate <horiuchi.nathan@gmail.com>
2022-02-11 15:28:18 -1000

    Stub functions for evaluating each move

--- src/engine/ai/Deterministic.ts
+++ src/engine/ai/Deterministic.ts
@@ -11,6 +11,35 @@ export class Deterministic extends AI {
     }
 
     evaluateMove(piece: Piece, move: string, board: BoardMap, fen: FEN, pieces: Piece[]): number {
+        if (piece.name === 'Pawn') {
+            return this.evaluatePawn(piece, move, board, fen, pieces);
+        }
+        else if (piece.name === 'Knight') {
+            return this.evaluateKnight(piece, move, board, fen, pieces);
+        }
+    }
+
+    evaluatePawn(piece: Piece, move: string, board: BoardMap, fen: FEN, pieces: Piece[]): number {
+        return 1;
+    }
+
+    evaluateKnight(piece: Piece, move: string, board: BoardMap, fen: FEN, pieces: Piece[]): number {
+        return 1;
+    }
+
+    evaluateBishop(piece: Piece, move: string, board: BoardMap, fen: FEN, pieces: Piece[]): number {
+        return 1;
+    }
+
+    evaluateRook(piece: Piece, move: string, board: BoardMap, fen: FEN, pieces: Piece[]): number {
+        return 1;
+    }
+
+    evaluateQueen(piece: Piece, move: string, board: BoardMap, fen: FEN, pieces: Piece[]): number {
+        return 1;
+    }
+
+    evaluateKing(piece: Piece, move: string, board: BoardMap, fen: FEN, pieces: Piece[]): number {
         return 1;
     }
 }
