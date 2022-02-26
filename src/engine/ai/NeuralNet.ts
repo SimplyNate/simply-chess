bffc58d4c38eb23c974f59ca0aa32f4883468e34
SimplyNate <horiuchi.nathan@gmail.com>
2022-02-25 21:59:01 -1000

    Neural network predicts moves correctly, model is currently weak

--- src/engine/ai/NeuralNet.ts
+++ src/engine/ai/NeuralNet.ts
@@ -19,34 +19,37 @@ export class NeuralNet extends AI {
     }
 
     async evaluateMove(piece: Piece, move: string): Promise<number> {
-        const isPawnPromotion = piece.name === 'Pawn' && (move.includes('8') || move.includes('1'));
-        let uciMove = `${piece.position}${move}`.replaceAll('-', '');
-        if (isPawnPromotion) {
-            if (piece.color === 'light') {
-                uciMove += 'Q';
+        if (this.model) {
+            const isPawnPromotion = piece.name === 'Pawn' && (move.includes('8') || move.includes('1'));
+            let uciMove = `${piece.position}${move}`.replaceAll('-', '');
+            if (isPawnPromotion) {
+                if (piece.color === 'light') {
+                    uciMove += 'Q';
+                }
+                else {
+                    uciMove += 'q';
+                }
             }
-            else {
-                uciMove += 'q';
-            }
+            /*
+            const body = JSON.stringify({ fen: this.fenString, move: [uciMove] });
+            const output = await fetch('http://127.0.0.1:8000/predict', {
+                method: 'POST',
+                body: body,
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const score = await output.json();
+             */
+            const encodedFen = convertFenToOneHot(this.fen);
+            const encodedMove = convertMoveToOneHot(uciMove);
+            const input = [...encodedFen, ...encodedMove];
+            const tensor = tf.tensor(input, [829], 'int32').expandDims(0);
+            const output = this.model.predict(tensor);
+            // @ts-ignore
+            return await output.data();
         }
-        /*
-        const body = JSON.stringify({ fen: this.fenString, move: [uciMove] });
-        const output = await fetch('http://127.0.0.1:8000/predict', {
-            method: 'POST',
-            body: body,
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        const score = await output.json();
-         */
-        const encodedFen = convertFenToOneHot(this.fen);
-        const encodedMove = convertMoveToOneHot(uciMove);
-        const input = [...encodedFen, ...encodedMove];
-        const tensor = tf.tensor(input, [829], 'int32').expandDims(0);
-        const output = this.model?.predict(tensor);
-        console.log(output);
-        return Number(output);
+        return 0;
     }
 }
