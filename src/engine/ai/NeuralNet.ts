489286ff5ee9924891f9b84945e44ea8a07fe2a6
SimplyNate <horiuchi.nathan@gmail.com>
2022-03-06 09:46:40 -1000

    Split remote and local evaluation methods into functions, simplyify

--- src/engine/ai/NeuralNet.ts
+++ src/engine/ai/NeuralNet.ts
@@ -1,7 +1,7 @@
-import AI, { MoveEvaluation } from './AI';
-import { Color, Piece } from '../pieces/Piece';
+import AI, {MoveEvaluation} from './AI';
+import {Color, Piece} from '../pieces/Piece';
 import * as tf from '@tensorflow/tfjs';
-import { convertFenToOneHot, convertMoveToOneHot } from '@/engine/ai/encoder';
+import {convertFenToOneHot, convertMoveToOneHot} from '@/engine/ai/encoder';
 
 export class NeuralNet extends AI {
     model: Readonly<tf.LayersModel> | null = null;
@@ -36,38 +36,59 @@ export class NeuralNet extends AI {
                     uciMoves.push(uciMove);
                 }
             }
-            /*
-            const body = JSON.stringify({ fen: this.fenString, moves: uciMoves });
-            const output = await fetch('http://127.0.0.1:8000/predict', {
-                method: 'POST',
-                body: body,
-                mode: 'cors',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            const score = await output.json();
-             */
-            const encodedFen = convertFenToOneHot(this.fen);
-            const moveEvaluations = [];
-            const preprocessed = [];
-            for (const move of uciMoves) {
-                const encodedMove = convertMoveToOneHot(move);
-                const input = [...encodedFen, ...encodedMove];
-                preprocessed.push(input);
-                const from = `${move[0]}-${move[1]}`;
-                const to = `${move[2]}-${move[3]}`;
-                moveEvaluations.push({ from, to, score: 0 });
-            }
-            const tensor = tf.tensor(preprocessed, [preprocessed.length, 829], 'int32');
-            const output = this.model.predict(tensor);
-            // @ts-ignore
-            const values = await output.data();
-            for (let i = 0; i < values.length; i++) {
-                moveEvaluations[i].score = values[i];
-            }
-            return moveEvaluations;
+            // return await this.remoteEvaluate(uciMoves);
+            return await this.localEvaluate(uciMoves);
         }
         return [];
+    }
+
+    createUnscoredEvaluations(uciMoves: string[]): MoveEvaluation[] {
+        const moveEvaluations = [];
+        for (const move of uciMoves) {
+            const from = `${move[0]}-${move[1]}`;
+            const to = `${move[2]}-${move[3]}`;
+            moveEvaluations.push({ from, to, score: 0 });
+        }
+        return moveEvaluations;
+    }
+
+    async localEvaluate(uciMoves: string[]): Promise<MoveEvaluation[]> {
+        const encodedFen = convertFenToOneHot(this.fen);
+        const moveEvaluations = [];
+        const preprocessed = [];
+        for (const move of uciMoves) {
+            const encodedMove = convertMoveToOneHot(move);
+            const input = [...encodedFen, ...encodedMove];
+            preprocessed.push(input);
+            const from = `${move[0]}-${move[1]}`;
+            const to = `${move[2]}-${move[3]}`;
+            moveEvaluations.push({ from, to, score: 0 });
+        }
+        const tensor = tf.tensor(preprocessed, [preprocessed.length, 829], 'int32');
+        const output = this.model?.predict(tensor);
+        // @ts-ignore
+        const values = await output.data();
+        for (let i = 0; i < values.length; i++) {
+            moveEvaluations[i].score = values[i];
+        }
+        return moveEvaluations;
+    }
+
+    async remoteEvaluate(uciMoves: string[]): Promise<MoveEvaluation[]> {
+        const body = JSON.stringify({ fen: this.fenString, moves: uciMoves });
+        const output = await fetch('http://127.0.0.1:8000/predict', {
+            method: 'POST',
+            body: body,
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const scores = await output.json() as number[];
+        const moveEvaluations = this.createUnscoredEvaluations(uciMoves);
+        for (let i = 0; i < moveEvaluations.length; i++) {
+            moveEvaluations[i].score = scores[i];
+        }
+        return moveEvaluations;
     }
 }
